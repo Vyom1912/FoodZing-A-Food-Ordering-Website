7679d3c6fdb05b8aa68b012de9a46fac4e50a549
VYOM PATEL <vyom1912@gmail.com>
2025-07-19 12:15:31 +0530

    Updated CSS styles

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ✅ CHANGED
 import StoreContextProvider from "./context/StoreContext.jsx";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename='/sample'>
+  <HashRouter>
+    {" "}
+    {/* ✅ CHANGED */}
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
